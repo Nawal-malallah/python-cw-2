7e6f66ebd127a2088e32d25290d3f68dd7619bcd
Nawal-malallah <108483676+Nawal-malallah@users.noreply.github.com>
2023-08-26 19:09:58 +0300

    cp

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-MyName = input("enter your name please")
-MyAge = int(input("enter age please"))
+MyName = input("enter your name please  ")
+MyAge = int(input("enter age please  "))
 
 print(f"My name is {MyName} and I am {MyAge} years old")
 
-No1 = int(input("Enter number"))
-No2 = int(input("Enter another number"))
+No1 = int(input("Enter number  "))
+No2 = int(input("Enter another number  "))
 op = input("please choose an operation / * - +")
 
 if op == "/":
@@ -19,12 +19,12 @@ else:
     print("what is wrong with u")
 
 bus_capacity = 30
-people_inbus = input("enter people boarding please")
+people_inbus = int(input("enter people boarding please  "))
 empty_seats = bus_capacity - people_inbus
 
-if empty_seats < people_inbus:
+if empty_seats > people_inbus:
     print("there is room in the bus")
 elif empty_seats == people_inbus:
     print("bus is full")
 else:
-    print(there is too many people kill them)
+    print("there is too many people kill them")
