bb18267387394d1d0e9b22f94bb05129460d0013
Nawal-malallah <108483676+Nawal-malallah@users.noreply.github.com>
2023-08-26 19:30:17 +0300

    ch

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ print(f"My name is {MyName} and I am {MyAge} years old")
 
 No1 = int(input("Enter number  "))
 No2 = int(input("Enter another number  "))
-op = input("please choose an operation / * - +")
+op = input("please choose an operation / * - +  ")
 x = No1 * No2
 d = No1/No2
 p = No1 + No2
@@ -13,12 +13,16 @@ m = No1 - No2
 
 if op == "/":
     print(f"{No1}/{No2} = {d}") 
+    print(No1/No2) 
 elif op == "*":
     print(f"{No1}*{No2} = {x}")
+    print(No1*No2)
 elif op == "-":
     print(f"{No1}-{No2} = {m}")
+    print(No1-No2)
 elif op == "+":
     print(f"{No1}+{No2} = {p}")
+    print(No1+No2)
 else:
     print("what is wrong with u")
 
@@ -27,8 +31,8 @@ people_inbus = int(input("enter people boarding please  "))
 empty_seats = bus_capacity - people_inbus
 
 if empty_seats > people_inbus:
-    print("there is room in the bus")
+    print(f"there is room in the bus there is {empty_seats}")
 elif empty_seats == people_inbus:
-    print("bus is full")
+    print("bus is full there is 40 ppl")
 else:
     print("there is too many people kill them")
